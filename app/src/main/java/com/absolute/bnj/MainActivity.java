9afc50f69868900cac0d47c3cd3984e2599bda13
Badrinathjanghel <badrinathjanghel@gmail.com>
2023-11-23 17:04:22 +0530

    build 3 updated, Splash screen content updated, API Level 33 updated. bnjasus

--- app/src/main/java/com/absolute/bnj/MainActivity.java
+++ app/src/main/java/com/absolute/bnj/MainActivity.java
@@ -7,6 +7,10 @@ import static com.absolute.bnj.Constants.FEED_SERVICES_URL;
 import static com.absolute.bnj.Constants.LOGIN_URL;
 import static com.absolute.bnj.Constants.SPLASH_URL;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -14,6 +18,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.net.Uri;
@@ -33,6 +38,12 @@ import com.airbnb.lottie.LottieAnimationView;
 import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.appupdate.AppUpdateOptions;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 
@@ -49,6 +60,7 @@ public class MainActivity extends Activity {
 
     Session session;
     String fbtoken="";
+    public static int APP_UPDATE_REQUEST_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -180,6 +192,48 @@ public class MainActivity extends Activity {
             loading_layout.setVisibility(View.GONE);
 //            lottie_graph.loop(false);
             myWebView.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+
+    //Check In-App Update API Google Play
+    private void checkForAppUpdate(){
+        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(context);
+
+        // Returns an intent object that you use to check for an update.
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                // This example applies an immediate update. To apply a flexible update
+                // instead, pass in AppUpdateType.FLEXIBLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+
+                // Request the update.
+                try {
+                    appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, this, APP_UPDATE_REQUEST_CODE);
+                } catch (IntentSender.SendIntentException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == APP_UPDATE_REQUEST_CODE){
+            if(resultCode != RESULT_OK){
+                Log.d(TAG, "onActivityResult: Update Failed "+ resultCode);
+            }else{
+                Log.d(TAG, "onActivityResult: Update Success....");
+            }
+
         }
     }
 }
